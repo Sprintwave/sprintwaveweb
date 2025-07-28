a74a4b0109de68095b743c3904e125a6d063d7c1
Joseph Scott <sprintwave@Josephs-Air-2.localdomain>
2025-07-28 11:23:13 +0100

    added blog

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=False, host='0.0.0.0', port=port)
