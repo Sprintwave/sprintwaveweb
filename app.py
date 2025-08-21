27354117af23003b243aab080d3a1a5fc89d735d
Joseph Scott <sprintwave@Josephs-MacBook-Air-2.local>
2025-08-21 14:38:54 +0100

    starlink

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ def home():
 def blog():
     return render_template('blog.html')
 
+@app.route('/starlink')
+def starlink():
+    return render_template('starlink.html')
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=False, host='0.0.0.0', port=port)
