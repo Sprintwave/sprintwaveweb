1ab3dd009d4a0a95a3010c184d9ba779111823d9
Joseph Scott <sprintwave@Josephs-MacBook-Air-2.local>
2025-08-21 17:00:52 +0100

    site map

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 import os
+from datetime import datetime
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -13,6 +15,47 @@ def blog():
 @app.route('/starlink')
 def starlink():
     return render_template('starlink.html')
+
+@app.route('/sitemap.xml')
+def sitemap():
+    """Generate sitemap XML for Google Search Console"""
+    
+    # Get current date in ISO format
+    current_date = datetime.now().strftime('%Y-%m-%d')
+    
+    sitemap_xml = f"""<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>https://www.sprintwave.co.uk/</loc>
+        <lastmod>{current_date}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
+    </url>
+    <url>
+        <loc>https://www.sprintwave.co.uk/starlink</loc>
+        <lastmod>{current_date}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.9</priority>
+    </url>
+    <url>
+        <loc>https://www.sprintwave.co.uk/blog</loc>
+        <lastmod>{current_date}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>
+</urlset>"""
+    
+    return Response(sitemap_xml, mimetype='application/xml')
+
+@app.route('/robots.txt')
+def robots():
+    """Generate robots.txt file"""
+    robots_txt = """User-agent: *
+Allow: /
+
+Sitemap: https://www.sprintwave.co.uk/sitemap.xml
+"""
+    return Response(robots_txt, mimetype='text/plain')
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
